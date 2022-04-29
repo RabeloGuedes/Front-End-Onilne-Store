aec7a05ca931fb3f3c41b6e5d0d59036cb4f71cd
Isaac Farias <riderzerotg@gmail.com>
2022-04-29 16:29:28 -0300

    refatora requerimento 10

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -33,5 +33,3 @@ export default class Cart extends Component {
     );
   }
 }
-
-// agora vai
--- src/services/cartFunc.js
+++ src/services/cartFunc.js
@@ -1,27 +1,21 @@
 export function addToCart(element) {
   element.quantity = 1;
+  let items = [];
   if (localStorage.getItem('cartItems')) {
-    const items = JSON.parse(localStorage.getItem('cartItems'));
+    items = JSON.parse(localStorage.getItem('cartItems'));
     if (items.some((i) => i.id === element.id)) {
-      const newObj = items.find((i) => i.id === element.id);
-      newObj.quantity += 1;
-      const teste = items.find((i) => i.id === newObj.id && i
-        .quantity <= newObj.quantity);
-      const remove = items.indexOf(teste);
-      items.splice(remove, 1);
-      items.push(newObj);
+      items.find((i) => i.id === element.id).quantity += 1;
+      console.log(items);
     } else {
       items.push(element);
     }
-    localStorage.setItem('cartItems', JSON.stringify(items));
   } else {
-    const items = [];
     items.push(element);
-    localStorage.setItem('cartItems', JSON.stringify(items));
   }
+  localStorage.setItem('cartItems', JSON.stringify(items));
 }
 
-export function removeToCart(element) {
+export function removeItem(element) {
   const items = JSON.parse(localStorage.getItem('cartItems'));
   const remove = items.indexOf(element);
   items.splice(remove, 1);
@@ -32,5 +26,3 @@ export function showCartItems() {
   const items = JSON.parse(localStorage.getItem('cartItems'));
   return items;
 }
-
-// agora vai
